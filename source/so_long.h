286c4d841a1167b24c70ff49f28cee3246ad4b55
alphagameX <arthurtweak@gmail.com>
2021-06-24 17:02:05 +0200

    ptn de norme de merde

--- source/so_long.h
+++ source/so_long.h
@@ -6,34 +6,33 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 15:05:46 by arthur            #+#    #+#             */
-/*   Updated: 2021/06/24 16:12:43 by arthur           ###   ########.fr       */
+/*   Updated: 2021/06/24 16:45:23 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-#ifndef	SO_LONG_H
-#define SO_LONG_H
 # include "../libft/libft.h"
 # include "so_long_struct.h"
 # include "so_long_norm_struct.h"
 # include <fcntl.h>
-
 
 /*
  ** DRAW
  **
  */
 
-void pixel_put(t_data *data, int x, int y, int color);
-int draw_map(t_game *game);
-char *get_color(char **addr, int width, int x, int y);
-int draw_player(t_game *game);
-int move_chunk_right(t_game *game);
-int move_chunk_left(t_game *game);
-void draw_hud(t_game *game);
-void draw_first_block(t_game *game, t_draw_block i);
-void draw_block(t_game *game, t_draw_block i);
-void draw_last_block(t_game *game, int x, int block);
-int get_block_type(t_map map, int x, int y);
+void    pixel_put(t_data *data, int x, int y, int color);
+int     draw_map(t_game *game);
+char    *get_color(char **addr, int width, int x, int y);
+int     draw_player(t_game *game);
+int     move_chunk_right(t_game *game);
+int     move_chunk_left(t_game *game);
+void    draw_hud(t_game *game);
+void    ddraw_first_block(t_game *game, t_draw_block i);
+void    draw_block(t_game *game, t_draw_block i);
+void    draw_last_block(t_game *game, int x, int block);
+int     get_block_type(t_map map, int x, int y);
 
 /*
  ** SARRAY
